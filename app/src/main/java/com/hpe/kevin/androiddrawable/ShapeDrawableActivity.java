7d53a29394cfc713be2653c5e2b3633006f38ff7
Kevin <momocha0625@126.com>
2018-11-19 12:34:18 +0800

    Shape Drawable

--- app/src/main/java/com/hpe/kevin/androiddrawable/ShapeDrawableActivity.java
+++ app/src/main/java/com/hpe/kevin/androiddrawable/ShapeDrawableActivity.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.TextView;
 
 public class ShapeDrawableActivity extends Activity {
@@ -37,5 +38,6 @@ public class ShapeDrawableActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_shape_drawable);
+//        findViewById(R.id.dashLine).setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 }
